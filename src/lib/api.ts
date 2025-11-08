a236521c73306f9d1965edd949fa313e706deea1
shivanshunigam01 <shivanshunigam8@gmail.com>
2025-11-08 21:15:57 +0530

    asdasds

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/leads";
+const API_URL = "https://api.truehospitals.com/leads";
 
 export const getLeads = async () => (await axios.get(API_URL)).data.leads;
 export const createLead = async (lead: any) =>
